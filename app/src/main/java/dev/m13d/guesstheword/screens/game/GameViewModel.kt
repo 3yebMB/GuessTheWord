a8c532ba4e38c39de3a35cd58840e579e1ea2384
Michael <3yebmb@gmail.com>
2019-05-25 13:16:43 +0300

    move code to viewmodel

--- app/src/main/java/dev/m13d/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/dev/m13d/guesstheword/screens/game/GameViewModel.kt
@@ -18,21 +18,74 @@ package dev.m13d.guesstheword.screens.game
 
 // TODO (02) Create the GameViewModel class, extending ViewModel
 // TODO (03) Add init and override onCleared; Add log statements to both
-import android.util.Log
 import androidx.lifecycle.ViewModel
-
 
 /**
  * ViewModel containing all the logic needed to run the game
  */
 class GameViewModel : ViewModel() {
+    // The current word
+    var word = ""
+
+    // The current score
+    var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
 
     init {
-        Log.i("GameViewModel", "GameViewModel created!")
+        resetList()
+        nextWord()
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed!")
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            // gameFinished() should happen here
+        } else {
+            word = wordList.removeAt(0)
+        }
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score++
+        nextWord()
     }
 }
